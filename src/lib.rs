55b324c740457447a2f8bc8259decb7f1b8d8255
Ernest Duckworth IV <31933290+ernman37@users.noreply.github.com>
2025-12-21 23:22:14 -0700

    create factory, basic decks and tests (#23)

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,30 @@ pub use objects::card::Color;
 pub use objects::card::Suit;
 pub use objects::card::Rank;
 pub use objects::card::Card;
+
+pub mod traits;
+
+pub use traits::factory::DeckFactory;
+
+pub mod utils;
+
+pub use utils::standard::Standard52;
+pub use utils::standard::Standard54;
+
+
+#[cfg(test)]
+mod tests {
+    use super::{Deck, Standard52, Standard54};
+
+    #[test]
+    fn test_deck_from_standard_52() {
+        let deck = Deck::from_factory(Standard52);
+        assert_eq!(deck.len(), 52);
+    }
+
+    #[test]
+    fn test_deck_from_standard_54() {
+        let deck = Deck::from_factory(Standard54);
+        assert_eq!(deck.len(), 54);
+    }
+}
---
+++ src/traits/factory.rs
@@ -0,0 +1,8 @@
+
+use crate::Card;
+use std::collections::VecDeque;
+
+/// Trait for generating the cards used within a Deck
+pub trait DeckFactory {
+    fn generate(&self) -> VecDeque<Card>;
+}
