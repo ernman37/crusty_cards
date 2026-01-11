b72a84b1f23a4c4629604b1775ab87b85a9986f5
ernman37 <Rion.duckworth@gmail.com>
2026-01-11 16:05:44 -0700

    factory docs

--- src/traits/factory.rs
+++ src/traits/factory.rs
@@ -1,7 +1,45 @@
 use crate::Card;
 use std::collections::VecDeque;
 
-/// Trait for generating the cards used within a Deck
+/// A trait for generating the cards in a [`Deck`](crate::Deck).
+///
+/// Implement this trait to create custom deck configurations for different games.
+///
+/// # Built-in Factories
+///
+/// - [`Standard52`](crate::Standard52) - Standard 52-card deck
+/// - [`Standard54`](crate::Standard54) - 54-card deck with 2 jokers
+///
+/// # Examples
+///
+/// ```rust
+/// use crusty_cards::{Card, Deck, DeckFactory, Suit, Rank};
+/// use std::collections::VecDeque;
+///
+/// /// A 48-card Pinochle deck (9-A, two of each)
+/// struct PinochleDeck;
+///
+/// impl DeckFactory for PinochleDeck {
+///     fn generate(&self) -> VecDeque<Card> {
+///         let ranks = [Rank::Nine, Rank::Ten, Rank::Jack, Rank::Queen, Rank::King, Rank::Ace];
+///         let mut cards = VecDeque::new();
+///         for _ in 0..2 {  // Two copies of each card
+///             for suit in Suit::ALL {
+///                 for rank in ranks {
+///                     cards.push_back(Card::new(suit, rank));
+///                 }
+///             }
+///         }
+///         cards
+///     }
+/// }
+///
+/// let deck = Deck::from_factory(PinochleDeck);
+/// assert_eq!(deck.len(), 48);
+/// ```
 pub trait DeckFactory {
+    /// Generates the cards for a deck.
+    ///
+    /// Returns a `VecDeque<Card>` containing all cards in the desired order.
     fn generate(&self) -> VecDeque<Card>;
 }
